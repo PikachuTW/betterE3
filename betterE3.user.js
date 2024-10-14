568c4b1ee8e518d37990aa7d60ebf3adfbc06283
YenTing Chen <82265690+PikachuTW@users.noreply.github.com>
2024-10-14 10:47:01 +0800

    update language and fix bugs

--- betterE3.user.js
+++ betterE3.user.js
@@ -11,16 +11,26 @@
 (() => {
     'use strict';
     const newList = [];
+    let language = 'tw';
+    if (document.querySelector('div.popover-region:nth-child(3) > a').classList.contains('semi-transparent')) language = 'en';
     for (let pageNum = 1; ; pageNum += 1) {
         const page = document.querySelector(`#news_page_${pageNum}`);
         if (!page) break;
         document.querySelectorAll(`#news_page_${pageNum} > ul:nth-child(1) > li`).forEach((element) => {
             let date = new Date();
-            const parsed = element.textContent.match(/(\d+)月 (\d+)日,(\d+):(\d+)*/);
-            date.setMonth(parseInt(parsed[1]) - 1);
-            date.setDate(parseInt(parsed[2]));
-            date.setHours(parseInt(parsed[3]));
-            date.setMinutes(parseInt(parsed[3]));
+            if (language == 'tw') {
+                const parsed = element.textContent.match(/(\d+)月 (\d+)日,(\d+):(\d+)*/);
+                date.setMonth(parseInt(parsed[1]) - 1);
+                date.setDate(parseInt(parsed[2]));
+                date.setHours(parseInt(parsed[3]));
+                date.setMinutes(parseInt(parsed[4]));
+            } else {
+                const parsed = element.textContent.match(/(\d+) (\w+),(\d+):(\d+)/);
+                date.setDate(parseInt(parsed[1]));
+                date.setMonth(new Date(Date.parse(parsed[2] + " 1, 2024")).getMonth());
+                date.setHours(parseInt(parsed[3]));
+                date.setMinutes(parseInt(parsed[4]));
+            }
             newList.push({
                 date,
                 innerHTML: element.innerHTML,
